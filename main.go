233e654a03e279a0b470acebddac8a56f6a5618a
Nir <niry@redhat.com>
2020-08-19 12:06:38 +0300

    adding some yamls for deploying

--- main.go
+++ main.go
@@ -20,15 +20,14 @@ import (
 	"flag"
 	"os"
 
+	"github.com/n1r1/poison-pill/controllers"
+	machinev1beta1 "github.com/openshift/machine-api-operator/pkg/apis/machine/v1beta1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	machineopenshiftiov1beta1 "github.com/n1r1/poison-pill/api/v1beta1"
-	"github.com/n1r1/poison-pill/controllers"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -40,7 +39,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
-	utilruntime.Must(machineopenshiftiov1beta1.AddToScheme(scheme))
+	utilruntime.Must(machinev1beta1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -50,7 +49,7 @@ func main() {
 	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
 		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
+			"Enabling this will ensure there is only o`ne active controller manager.")
 	flag.Parse()
 
 	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
