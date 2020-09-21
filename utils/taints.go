092aeacac4bd902cf0833bec28bb65b820524abd
Nir <niry@redhat.com>
2020-09-21 22:27:11 +0300

    added unschedulable taint for unhealthy node peer reports unhealthy only if time exists in annotation
    Signed-off-by: Nir <niry@redhat.com>

---
+++ utils/taints.go
@@ -0,0 +1,27 @@
+package utils
+
+import v1 "k8s.io/api/core/v1"
+
+// TaintExists checks if the given taint exists in list of taints. Returns true if exists false otherwise.
+func TaintExists(taints []v1.Taint, taintToFind *v1.Taint) bool {
+	for _, taint := range taints {
+		if taint.MatchTaint(taintToFind) {
+			return true
+		}
+	}
+	return false
+}
+
+// DeleteTaint removes all the taints that have the same key and effect to given taintToDelete.
+func DeleteTaint(taints []v1.Taint, taintToDelete *v1.Taint) ([]v1.Taint, bool) {
+	newTaints := []v1.Taint{}
+	deleted := false
+	for i := range taints {
+		if taintToDelete.MatchTaint(&taints[i]) {
+			deleted = true
+			continue
+		}
+		newTaints = append(newTaints, taints[i])
+	}
+	return newTaints, deleted
+}
