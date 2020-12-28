d529a73cd1f8fbff7a4d1c2ab8cef6786bc2d41f
Nir <niry@redhat.com>
2020-12-28 16:21:45 +0200

    Add defer in tests so Gingko can catch failure inside the go routine
    Signed-off-by: Nir <niry@redhat.com>

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -97,6 +97,7 @@ var _ = BeforeSuite(func(done Done) {
 	myNodeName = "node1"
 
 	go func() {
+                defer GinkgoRecover()
 		err = k8sManager.Start(ctrl.SetupSignalHandler())
 		Expect(err).ToNot(HaveOccurred())
 	}()
