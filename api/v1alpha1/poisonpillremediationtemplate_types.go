17840505d1939b7a2c505f6ed3a2a14dcaff0d06
Nir <niry@redhat.com>
2021-04-25 17:49:29 +0300

    Add the poison pill remediation template field to the PoisonPillRemdiatioTemplate CRD
    Signed-off-by: Nir <niry@redhat.com>

--- api/v1alpha1/poisonpillremediationtemplate_types.go
+++ api/v1alpha1/poisonpillremediationtemplate_types.go
@@ -23,11 +23,16 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
+type PoisonPillRemediationTemplateResource struct {
+	Spec PoisonPillRemediationSpec `json:"spec"`
+}
+
 // PoisonPillRemediationTemplateSpec defines the desired state of PoisonPillRemediationTemplate
 type PoisonPillRemediationTemplateSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
+	Template PoisonPillRemediationTemplateResource `json:"template"`
 }
 
 // PoisonPillRemediationTemplateStatus defines the observed state of PoisonPillRemediationTemplate
