f7e856491db9eac36cd0ca85b79e816df95038bf
Marc Sluiter <msluiter@redhat.com>
2021-05-11 13:26:52 +0200

    Introduce gRPC and certificates for peer health check
    Certs are created and stored in a Secret by the manager if
    they don't exist yet, and read from the Secret by the daemonset
    
    Signed-off-by: Marc Sluiter <msluiter@redhat.com>

---
+++ pkg/certificates/storage.go
@@ -0,0 +1,112 @@
+package certificates
+
+import (
+	"bytes"
+	"context"
+	"sync"
+
+	"github.com/go-logr/logr"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/pointer"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type CertStorageReader interface {
+	GetCerts() (caPem, certPem, keyPem *bytes.Buffer, err error)
+}
+
+// for tests only
+var _ CertStorageReader = &MemoryCertStorage{}
+
+type MemoryCertStorage struct {
+	CaPem, CertPem, KeyPem *bytes.Buffer
+}
+
+func (m *MemoryCertStorage) GetCerts() (caPem, certPem, keyPem *bytes.Buffer, err error) {
+	return m.CaPem, m.CertPem, m.KeyPem, nil
+}
+
+const (
+	secretName = "poison-pill-certificates"
+	caPemKey   = "caPem"
+	certPemKey = "certPem"
+	keyPemKey  = "keyPem"
+)
+
+var _ CertStorageReader = &SecretCertStorage{}
+
+type SecretCertStorage struct {
+	client.Client
+	log       logr.Logger
+	namespace string
+	secret    *v1.Secret
+	mutex     sync.Mutex
+}
+
+//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
+
+func NewSecretCertStorage(c client.Client, log logr.Logger, namespace string) *SecretCertStorage {
+	return &SecretCertStorage{
+		Client:    c,
+		log:       log,
+		namespace: namespace,
+		mutex:     sync.Mutex{},
+	}
+}
+
+func (s *SecretCertStorage) GetCerts() (caPem, certPem, keyPem *bytes.Buffer, err error) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if s.secret == nil {
+		certSecret := &v1.Secret{}
+		key := types.NamespacedName{
+			Namespace: s.namespace,
+			Name:      secretName,
+		}
+		if err := s.Get(context.Background(), key, certSecret); err != nil {
+			return nil, nil, nil, err
+		}
+		s.secret = certSecret
+	}
+
+	toBuffer := func(key string) *bytes.Buffer {
+		b := &bytes.Buffer{}
+		b.Write(s.secret.Data[key])
+		return b
+	}
+	caPem = toBuffer(caPemKey)
+	certPem = toBuffer(certPemKey)
+	keyPem = toBuffer(keyPemKey)
+	return
+}
+
+func (s *SecretCertStorage) StoreCerts(caPem, certPem, keyPem *bytes.Buffer) error {
+	secret := &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: s.namespace,
+			Name:      secretName,
+		},
+		Immutable: pointer.BoolPtr(true),
+		Data:      nil,
+		StringData: map[string]string{
+			caPemKey:   caPem.String(),
+			certPemKey: certPem.String(),
+			keyPemKey:  keyPem.String(),
+		},
+		Type: v1.SecretTypeOpaque,
+	}
+	if err := s.Create(context.Background(), secret); err != nil {
+		if errors.IsAlreadyExists(err) {
+			s.log.Info("certificates already stored in secret")
+			return nil
+		}
+		return err
+	}
+
+	return nil
+}
---
+++ pkg/certificates/suite_test.go
@@ -0,0 +1,67 @@
+package certificates
+
+import (
+	"path/filepath"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+)
+
+func TestCertificates(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecsWithDefaultAndCustomReporters(t,
+		"Certificates Suite",
+		[]Reporter{printer.NewlineReporter{}})
+}
+
+var cfg *rest.Config
+var k8sClient client.Client
+var testEnv *envtest.Environment
+
+var _ = BeforeSuite(func() {
+	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+
+	By("bootstrapping test environment")
+	testEnv = &envtest.Environment{
+		BinaryAssetsDirectory: "../../testbin/bin",
+		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
+		ErrorIfCRDPathMissing: true,
+	}
+
+	var err error
+	cfg, err = testEnv.Start()
+	Expect(err).NotTo(HaveOccurred())
+	Expect(cfg).NotTo(BeNil())
+
+	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme:             scheme.Scheme,
+		MetricsBindAddress: ":8082",
+	})
+	Expect(err).ToNot(HaveOccurred())
+
+	go func() {
+		defer GinkgoRecover()
+		err = k8sManager.Start(ctrl.SetupSignalHandler())
+		Expect(err).ToNot(HaveOccurred())
+	}()
+
+	k8sClient = k8sManager.GetClient()
+	Expect(k8sClient).ToNot(BeNil())
+
+}, 60)
+
+var _ = AfterSuite(func() {
+	By("tearing down the test environment")
+	err := testEnv.Stop()
+	Expect(err).NotTo(HaveOccurred())
+})
