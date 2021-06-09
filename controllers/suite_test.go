afaa8edb7c28e26317f0d80fbf7667b5dfc238f9
Marc Sluiter <msluiter@redhat.com>
2021-06-09 17:14:50 +0200

    Use readyz endpoint for api server check
    Signed-off-by: Marc Sluiter <msluiter@redhat.com>

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -29,7 +29,6 @@ import (
 
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
@@ -48,7 +47,6 @@ import (
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var cfg *rest.Config
 var k8sClient *K8sClientWrapper
 var testEnv *envtest.Environment
 var dummyDog watchdog.Watchdog
@@ -149,7 +147,7 @@ var _ = BeforeSuite(func() {
 
 	peers := peers.New(unhealthyNodeName, peerUpdateInterval, k8sClient, ctrl.Log.WithName("peers"))
 
-	apiCheck := apicheck.New(unhealthyNodeName, peers, rebooter, apiCheckInterval, maxErrorThreshold, k8sClient, ctrl.Log.WithName("api-check"))
+	apiCheck := apicheck.New(unhealthyNodeName, peers, rebooter, apiCheckInterval, maxErrorThreshold, cfg, ctrl.Log.WithName("api-check"))
 
 	timeToAssumeNodeRebooted := time.Duration(maxErrorThreshold) * apiCheckInterval
 	timeToAssumeNodeRebooted += dummyDog.GetTimeout()
