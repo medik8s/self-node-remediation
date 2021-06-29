a62bc3862d462bceb1ed6b884df1a10688742044
Roy Golan <rgolan@redhat.com>
2021-06-29 20:47:08 +0300

    Revert the config validating webhook
    This reverts commit d84fe26b3feb7c42ccc2ccc4f22c35abf06fce34.

--- api/v1alpha1/poisonpillconfig_types.go
+++ api/v1alpha1/poisonpillconfig_types.go
@@ -20,14 +20,14 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-const (
-	configCRName                        = "poison-pill-config"
-	watchdogFilePath                    = "/dev/watchdog1"
-	safeTimeToAssumeNodeRebootedSeconds = 180
-)
-
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+const (
+	configCRName                          = "poison-pill-config"
+	defaultWatchdogPath                   = "/dev/watchdog1"
+	defaultSafetToAssumeNodeRebootTimeout = 180
+)
 
 // PoisonPillConfigSpec defines the desired state of PoisonPillConfig
 type PoisonPillConfigSpec struct {
@@ -79,15 +79,12 @@ func init() {
 	SchemeBuilder.Register(&PoisonPillConfig{}, &PoisonPillConfigList{})
 }
 
-// NewDefaultPoisonPillConfig returns a config object with name and sepc fields set.
 func NewDefaultPoisonPillConfig() PoisonPillConfig {
 	return PoisonPillConfig{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: configCRName,
-		},
+		ObjectMeta: metav1.ObjectMeta{Name: configCRName},
 		Spec: PoisonPillConfigSpec{
-			WatchdogFilePath:                    watchdogFilePath,
-			SafeTimeToAssumeNodeRebootedSeconds: safeTimeToAssumeNodeRebootedSeconds,
+			WatchdogFilePath:                    defaultWatchdogPath,
+			SafeTimeToAssumeNodeRebootedSeconds: defaultSafetToAssumeNodeRebootTimeout,
 		},
 	}
 }
