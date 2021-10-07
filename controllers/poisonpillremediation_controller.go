4ab0e40f552737114cb0ba43a95201185eb3df1c
Nir <niry@redhat.com>
2021-10-07 14:11:36 +0300

    making sure `GetPoisonPillAgentPod()` return a non-nil err if the result is nil
    Signed-off-by: Nir <niry@redhat.com>

--- controllers/poisonpillremediation_controller.go
+++ controllers/poisonpillremediation_controller.go
@@ -179,8 +179,6 @@ func (r *PoisonPillRemediationReconciler) Reconcile(ctx context.Context, req ctr
 		return ctrl.Result{}, nil
 	}
 
-	hasRunningPoisonPillPod := false
-
 	pod, err := utils.GetPoisonPillAgentPod(node.Name, r.Client)
 
 	if err != nil {
@@ -188,14 +186,11 @@ func (r *PoisonPillRemediationReconciler) Reconcile(ctx context.Context, req ctr
 		return ctrl.Result{}, err
 	}
 
-	if pod != nil && pod.Status.Phase == v1.PodRunning {
-		hasRunningPoisonPillPod = true
-	}
-
 	//if the unhealthy node doesn't have the poison pill agent pod, the node might not reboot, and we might end up
 	//in deleting a running node
-	if !hasRunningPoisonPillPod {
-		r.logger.Error(errors.New("node is missing a running poison pill agent pod, which means the node might not reboot when we'll delete the node. Skipping remediation"), "")
+	if pod.Status.Phase != v1.PodRunning {
+		r.logger.Error(errors.New("node is missing a running poison pill agent pod, which means the node might not reboot when we'll delete the node. Skipping remediation"),
+			"pod is not in running phase", "pod phase", pod.Status.Phase)
 		return ctrl.Result{}, nil
 	}
 
