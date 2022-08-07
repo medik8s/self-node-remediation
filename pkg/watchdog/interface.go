1b31c224de61e9d1150cfc07359aa8b5e033b0a4
Michael Shitrit <mshitrit@redhat.com>
2022-08-07 11:32:37 +0300

    use one watchdog status instead of a couple of booleans vars
    Signed-off-by: Michael Shitrit <mshitrit@redhat.com>

--- pkg/watchdog/interface.go
+++ pkg/watchdog/interface.go
@@ -9,10 +9,8 @@ import (
 type Watchdog interface {
 	// Start should be called by the manager and block on the given context
 	Start(ctx context.Context) error
-	// IsStarted return if the watchdog is running
-	IsStarted() bool
-	// IsRebooting return if the watchdog has already stopped feeding and currently about to reboot the node
-	IsRebooting() bool
+	// Status shows current watchdog status
+	Status() watchdogStatus
 	// Stop stops feeding the watchdog, which results in a reboot of the node
 	Stop()
 	// GetTimeout returns the watchdog timeout when it reboots the node without feeding
