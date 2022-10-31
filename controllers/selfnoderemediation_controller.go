e84572112bb5a70804392bcdd8195799055400b6
Michael Shitrit <mshitrit@redhat.com>
2022-10-31 17:19:07 +0200

    Allow namespace update permissions
    Signed-off-by: Michael Shitrit <mshitrit@redhat.com>

--- controllers/selfnoderemediation_controller.go
+++ controllers/selfnoderemediation_controller.go
@@ -121,6 +121,7 @@ func (r *SelfNodeRemediationReconciler) SetupWithManager(mgr ctrl.Manager) error
 //+kubebuilder:rbac:groups=self-node-remediation.medik8s.io,resources=selfnoderemediations/finalizers,verbs=update
 //+kubebuilder:rbac:groups=core,resources=nodes,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=machine.openshift.io,resources=machines,verbs=get;list;watch
+//+kubebuilder:rbac:groups=core,resources=namespaces,verbs=update
 
 func (r *SelfNodeRemediationReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	r.logger = r.Log.WithValues("selfnoderemediation", req.NamespacedName)
