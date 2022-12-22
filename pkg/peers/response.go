9286d14a327b4488e6422dfef913b6f271492361
Michael Shitrit <mshitrit@redhat.com>
2022-12-22 14:13:19 +0200

    Fixing wrong scenario of control-plane reboot
    Signed-off-by: Michael Shitrit <mshitrit@redhat.com>

--- pkg/peers/response.go
+++ pkg/peers/response.go
@@ -8,11 +8,11 @@ type Response struct {
 type reason string
 
 const (
-	HealthyBecauseCRNotFound                           reason = "CR Not found, node is considered healthy"
-	HealthyBecauseErrorsThresholdNotReached            reason = "Errors number hasn't reached threshold not querying peers yet, node is considered healthy"
-	HealthyBecauseNoPeersResponseNotReachedMaxAttempts reason = "No response from peer hasn't passed the non responsive time threshold so still considered healthy"
-	HealthyBecauseNoPeersWereFound                     reason = "No Peers where found, node is considered healthy"
-	HealthyBecauseMostPeersCantAccessAPIServer         reason = "Most peers couldn't access API server, node is considered healthy"
+	HealthyBecauseCRNotFound                       reason = "CR Not found, node is considered healthy"
+	HealthyBecauseErrorsThresholdNotReached        reason = "Errors number hasn't reached threshold not querying peers yet, node is considered healthy"
+	HealthyBecauseNoPeersResponseNotReachedTimeout reason = "No response from peer hasn't passed the non responsive time threshold so still considered healthy"
+	HealthyBecauseNoPeersWereFound                 reason = "No Peers where found, node is considered healthy"
+	HealthyBecauseMostPeersCantAccessAPIServer     reason = "Most peers couldn't access API server, node is considered healthy"
 
 	UnHealthyBecausePeersResponse  reason = "Node is reported unhealthy by it's peers"
 	UnHealthyBecauseNodeIsIsolated reason = "Node is isolated, node is considered unhealthy"
