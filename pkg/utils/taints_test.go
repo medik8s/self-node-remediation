18d3bcf1dd315495852b45022e643b489f7a62b4
Michael Shitrit <mshitrit@redhat.com>
2023-06-12 09:58:35 +0300

    Adding unit tests and removing unnecessary var assignment
    Signed-off-by: Michael Shitrit <mshitrit@redhat.com>

---
+++ pkg/utils/taints_test.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"testing"
+
+	"k8s.io/apimachinery/pkg/version"
+)
+
+func Test_setOutOfTaintSupportedFlag(t *testing.T) {
+	type args struct {
+		version *version.Info
+	}
+	tests := []struct {
+		name                    string
+		args                    args
+		wantErr                 bool
+		isOutOfTaintFlagEnabled bool
+	}{
+		//valid use-cases
+		{name: "validEnabledNoPlus", args: args{&version.Info{Major: "1", Minor: "26"}}, wantErr: false, isOutOfTaintFlagEnabled: true},
+		{name: "validDisabledEnabledNoPlus", args: args{&version.Info{Major: "1", Minor: "24"}}, wantErr: false, isOutOfTaintFlagEnabled: false},
+		{name: "validEnabledWithPlus", args: args{&version.Info{Major: "1", Minor: "26+"}}, wantErr: false, isOutOfTaintFlagEnabled: true},
+		{name: "validDisabledWithPlus", args: args{&version.Info{Major: "1", Minor: "24+"}}, wantErr: false, isOutOfTaintFlagEnabled: false},
+		//invalid use-cases
+		{name: "inValidNoPlus", args: args{&version.Info{Major: "1", Minor: "%24"}}, wantErr: true, isOutOfTaintFlagEnabled: false},
+		{name: "inValidWithPlus", args: args{&version.Info{Major: "1+", Minor: "26"}}, wantErr: true, isOutOfTaintFlagEnabled: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			IsOutOfServiceTaintSupported = false
+			if err := setOutOfTaintSupportedFlag(tt.args.version); (err != nil) != tt.wantErr || IsOutOfServiceTaintSupported != tt.isOutOfTaintFlagEnabled {
+				t.Errorf("setOutOfTaintSupportedFlag() error = %v, wantErr %v, expected out of taint flag %v", err, tt.wantErr, tt.isOutOfTaintFlagEnabled)
+			}
+		})
+	}
+}
