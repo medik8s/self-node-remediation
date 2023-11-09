a2b121923a73f74b3651336e25c36ae43cb99bd4
Michael Shitrit <mshitrit@redhat.com>
2023-11-09 18:23:10 +0200

    Setting the default strategy to be Automatic (missed change due to another PR merge)
    Signed-off-by: Michael Shitrit <mshitrit@redhat.com>

--- api/v1alpha1/selfnoderemediation_types.go
+++ api/v1alpha1/selfnoderemediation_types.go
@@ -24,7 +24,7 @@ const (
 	AutomaticRemediationStrategy         = RemediationStrategyType("Automatic")
 	ResourceDeletionRemediationStrategy  = RemediationStrategyType("ResourceDeletion")
 	OutOfServiceTaintRemediationStrategy = RemediationStrategyType("OutOfServiceTaint")
-	DefaultRemediationStrategy           = ResourceDeletionRemediationStrategy
+	DefaultRemediationStrategy           = AutomaticRemediationStrategy
 	// ProcessingConditionType is the condition type used to signal NHC the remediation status
 	ProcessingConditionType = "Processing"
 	// SucceededConditionType is the condition type used to signal NHC whether the remediation was successful or not
