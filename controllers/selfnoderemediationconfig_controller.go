1cd207c90f2854ec8683bd82b31e269f866d198a
Michael Shitrit <mshitrit@redhat.com>
2024-01-16 10:16:47 +0200

    use info log in case of ds update conflict
    Signed-off-by: Michael Shitrit <mshitrit@redhat.com>

--- controllers/selfnoderemediationconfig_controller.go
+++ controllers/selfnoderemediationconfig_controller.go
@@ -160,7 +160,9 @@ func (r *SelfNodeRemediationConfigReconciler) syncConfigDaemonSet(ctx context.Co
 		}
 		err = r.syncK8sResource(ctx, snrConfig, obj)
 		if err != nil {
-			if !errors.IsConflict(err) {
+			if errors.IsConflict(err) {
+				logger.Info("Couldn't sync self-node-remediation daemons objects because ds is updated, retrying...")
+			} else {
 				logger.Error(err, "Couldn't sync self-node-remediation daemons objects")
 			}
 			return err
