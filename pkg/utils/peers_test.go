12c3601316ab05bb68f0fa47067b83fa0514029b
Marc Sluiter <msluiter@redhat.com>
2024-06-20 10:08:19 +0200

    Improve batch size test
    Signed-off-by: Marc Sluiter <msluiter@redhat.com>

--- pkg/utils/peers_test.go
+++ pkg/utils/peers_test.go
@@ -7,56 +7,18 @@ import (
 
 var _ = Describe("Utils/Peers tests", func() {
 
-	var nrOfNodes int
-
-	Context("small cluster", func() {
-		BeforeEach(func() {
-			nrOfNodes = 2
-		})
-		It("GetNextBatchSize should return correct first batch size", func() {
-			Expect(GetNextBatchSize(nrOfNodes, nrOfNodes)).To(Equal(2))
-		})
-		It("GetNrOfBatches should return correct nr of batches", func() {
-			Expect(GetNrOfBatches(nrOfNodes)).To(Equal(1))
-		})
-	})
-
-	Context("medium cluster", func() {
-		BeforeEach(func() {
-			nrOfNodes = 20
-			// expected batches: 3 + 5*3 + 2 = 7 batches
-		})
-		It("GetNextBatchSize should return correct first batch size", func() {
-			Expect(GetNextBatchSize(nrOfNodes, nrOfNodes)).To(Equal(3))
-		})
-		It("GetNextBatchSize should return correct next batch size", func() {
-			Expect(GetNextBatchSize(nrOfNodes, nrOfNodes-3)).To(Equal(3))
-		})
-		It("GetNextBatchSize should return correct last batch size", func() {
-			Expect(GetNextBatchSize(nrOfNodes, 2)).To(Equal(2))
-		})
-		It("GetNrOfBatches should return correct nr of batches", func() {
-			Expect(GetNrOfBatches(nrOfNodes)).To(Equal(7))
-		})
-	})
-
-	Context("big cluster", func() {
-		BeforeEach(func() {
-			nrOfNodes = 53
-			// expected batches: 3 + 10*5 = 11 batches
-		})
-		It("GetNextBatchSize should return correct first batch size", func() {
-			Expect(GetNextBatchSize(nrOfNodes, nrOfNodes)).To(Equal(3))
-		})
-		It("GetNextBatchSize should return correct next batch size", func() {
-			Expect(GetNextBatchSize(nrOfNodes, nrOfNodes-3)).To(Equal(5))
-		})
-		It("GetNextBatchSize should return correct last batch size", func() {
-			Expect(GetNextBatchSize(nrOfNodes, 0)).To(Equal(0))
-		})
-		It("GetNrOfBatches should return correct nr of batches", func() {
-			Expect(GetNrOfBatches(nrOfNodes)).To(Equal(11))
-		})
-	})
+	DescribeTable("Batch size", func(nrOfNodes int, expectedBatchSizes []int) {
+		Expect(GetNrOfBatches(nrOfNodes)).To(Equal(len(expectedBatchSizes)))
+		remainingNodes := nrOfNodes
+		for i := 0; remainingNodes > 0; i++ {
+			batchSize := GetNextBatchSize(nrOfNodes, remainingNodes)
+			Expect(batchSize).To(Equal(expectedBatchSizes[i]))
+			remainingNodes -= batchSize
+		}
+	},
+		Entry("small cluster", 2, []int{2}),
+		Entry("medium cluster", 20, []int{3, 3, 3, 3, 3, 3, 2}),
+		Entry("big cluster", 53, []int{3, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5}),
+	)
 
 })
