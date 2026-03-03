31d3213c9c4757a5e93017082e90c999f069fa35
Juris Krumins <juris.krumins80@gmail.com>
2026-03-03 11:25:00 +0000

    Make customDsNodeSelectors CRD schema a bit more stricter with required fields and possible values constraints.

--- api/v1alpha1/selfnoderemediationconfig_webhook.go
+++ api/v1alpha1/selfnoderemediationconfig_webhook.go
@@ -222,41 +222,47 @@ func validateCustomDsNodeSelectors(snrConfig *SelfNodeRemediationConfig) error {
 
 func validateNodeSelector(nodeSelector v1.NodeSelectorRequirement) error {
 	if len(nodeSelector.Key) == 0 {
-		err := fmt.Errorf("invalid key for nodeSelector term, key must be non-empty string")
-		selfNodeRemediationConfigLog.Error(err, "invalid key for nodeSelector term, key must be non-empty string")
+		err := fmt.Errorf("invalid key for nodeSelector: key must be non-empty")
+		selfNodeRemediationConfigLog.Error(err, "invalid key for nodeSelector: key must be non-empty")
 		return err
 	}
-	if len(nodeSelector.Operator) > 0 {
-		switch {
-		case nodeSelector.Operator == v1.NodeSelectorOpExists:
-		//Don't need to validate values
-		case nodeSelector.Operator == v1.NodeSelectorOpDoesNotExist:
-		//Don't need to validate values
-		case nodeSelector.Operator == v1.NodeSelectorOpIn || nodeSelector.Operator == v1.NodeSelectorOpNotIn:
-			// values need to be non-empty list of string values
-			if len(nodeSelector.Values) == 0 {
-				err := fmt.Errorf("invalid values for nodeSelector term, values must not be empty when Operator value is %s", nodeSelector.Operator)
-				selfNodeRemediationConfigLog.Error(err, "invalid values for nodeSelector term, values must not be empty for Operator value is", nodeSelector.Operator)
-				return err
-			}
-		case nodeSelector.Operator == v1.NodeSelectorOpGt || nodeSelector.Operator == v1.NodeSelectorOpLt:
-			// values array must have a single element, which will be interpreted as an integer
-			if len(nodeSelector.Values) != 1 {
-				err := fmt.Errorf("invalid values for nodeSelector term, array must have a single element, which will be interpreted as an integer when Operator value is %s", nodeSelector.Operator)
-				selfNodeRemediationConfigLog.Error(err, "invalid values for nodeSelector term, array must have a single element, which will be interpreted as an integer when Operator value is", nodeSelector.Operator)
-				return err
-			}
-			_, err := strconv.Atoi(nodeSelector.Values[0])
-			if err != nil {
-				err := fmt.Errorf("invalid value for nodeSelector term, first array element must be valid integer when Operator value is %s", nodeSelector.Operator)
-				selfNodeRemediationConfigLog.Error(err, "invalid value for nodeSelector term, first array element must be valid integer when Operator value is", nodeSelector.Operator)
-				return err
-			}
-		default:
-			err := fmt.Errorf("invalid operator for nodeSelector: %s", nodeSelector.Operator)
-			selfNodeRemediationConfigLog.Error(err, "invalid operator for nodeSelector", "valid values", []v1.NodeSelectorOperator{v1.NodeSelectorOpExists, v1.NodeSelectorOpDoesNotExist, v1.NodeSelectorOpIn, v1.NodeSelectorOpIn, v1.NodeSelectorOpGt, v1.NodeSelectorOpLt}, "received value", nodeSelector.Operator)
+	if len(nodeSelector.Operator) == 0 {
+		err := fmt.Errorf("invalid operator for nodeSelector: operator must be non-empty")
+		selfNodeRemediationConfigLog.Error(err, "invalid operator for nodeSelector: operator must be non-empty")
+		return err
+	}
+	switch nodeSelector.Operator {
+	case v1.NodeSelectorOpExists, v1.NodeSelectorOpDoesNotExist:
+		// values must not exist
+		if len(nodeSelector.Values) != 0 {
+			err := fmt.Errorf("invalid values for nodeSelector term, values must not exists when Operator value is %s", nodeSelector.Operator)
+			selfNodeRemediationConfigLog.Error(err, "invalid values for nodeSelector term, values must not exists when Operator value is", "operator", nodeSelector.Operator)
 			return err
 		}
+	case v1.NodeSelectorOpIn, v1.NodeSelectorOpNotIn:
+		// values need to be non-empty list of string values
+		if len(nodeSelector.Values) == 0 {
+			err := fmt.Errorf("invalid values for nodeSelector term, values must not be empty when Operator value is %s", nodeSelector.Operator)
+			selfNodeRemediationConfigLog.Error(err, "invalid values for nodeSelector term, values must not be empty for Operator value is", "operator", nodeSelector.Operator)
+			return err
+		}
+	case v1.NodeSelectorOpGt, v1.NodeSelectorOpLt:
+		// values array must have a single element, which will be interpreted as an integer
+		if len(nodeSelector.Values) != 1 {
+			err := fmt.Errorf("invalid values for nodeSelector term, array must have a single element, which will be interpreted as an integer when Operator value is %s", nodeSelector.Operator)
+			selfNodeRemediationConfigLog.Error(err, "invalid values for nodeSelector term, array must have a single element, which will be interpreted as an integer when Operator value is", "operator", nodeSelector.Operator)
+			return err
+		}
+		_, err := strconv.Atoi(nodeSelector.Values[0])
+		if err != nil {
+			err := fmt.Errorf("invalid value for nodeSelector term, first array element must be valid integer when Operator value is %s", nodeSelector.Operator)
+			selfNodeRemediationConfigLog.Error(err, "invalid value for nodeSelector term, first array element must be valid integer when Operator value is", "operator", nodeSelector.Operator)
+			return err
+		}
+	default:
+		err := fmt.Errorf("invalid operator for nodeSelector: %s", nodeSelector.Operator)
+		selfNodeRemediationConfigLog.Error(err, "invalid operator for nodeSelector", "valid values", []v1.NodeSelectorOperator{v1.NodeSelectorOpExists, v1.NodeSelectorOpDoesNotExist, v1.NodeSelectorOpIn, v1.NodeSelectorOpIn, v1.NodeSelectorOpGt, v1.NodeSelectorOpLt}, "received value", nodeSelector.Operator)
+		return err
 	}
 	return nil
 }
