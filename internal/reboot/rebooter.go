81e5596b805d40e297eaa411c9e5166d9c6d93ed
Pranav Gaikwad <pgaikwad@redhat.com>
2026-09-23 13:11:14 -0400

    :bug: re-enable software reboot, add reboot -f methods to reboot before trying sysrq
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>

--- internal/reboot/rebooter.go
+++ internal/reboot/rebooter.go
@@ -84,15 +84,25 @@ func (r *watchdogRebooter) Reboot() error {
 	}
 }
 
-// softwareReboot performs software reboot by running systemctl reboot
+// softwareReboot attempts multiple reboot methods in order, stopping at the
+// first that succeeds. reboot(2)-based methods are tried before sysrq because
+// they respect PID namespace isolation; sysrq bypasses it and is a last resort.
 func (r *watchdogRebooter) softwareReboot() error {
 	r.log.Info("about to try software reboot")
-	// privileged:true required to run this
-	rebootCmd := exec.Command("/usr/bin/nsenter", "-m/proc/1/ns/mnt", "/bin/bash", "-c", "echo b > /proc/sysrq-trigger")
-
-	if err := rebootCmd.Run(); err != nil {
-		r.log.Error(err, "failed to run reboot command")
-		// TODO retry because of this?
+	cmds := [][]string{
+		{"systemctl", "reboot", "--force", "--force"},
+		{"nsenter", "-m/proc/1/ns/mnt", "--", "systemctl", "reboot", "--force", "--force"},
+		{"reboot", "-f"},
+		{"nsenter", "-m/proc/1/ns/mnt", "--", "reboot", "-f"},
+		{"nsenter", "-m/proc/1/ns/mnt", "/bin/bash", "-c", "echo b > /proc/sysrq-trigger"},
+	}
+	for _, cmd := range cmds {
+		r.log.Info("trying reboot command", "command", cmd)
+		if err := exec.Command(cmd[0], cmd[1:]...).Run(); err != nil {
+			r.log.Error(err, "reboot command failed, trying next", "command", cmd)
+			continue
+		}
+		return nil
 	}
 	return nil
 }
